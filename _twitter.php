edd2fb2b093cc74ed26818d13b5ca76495fbbb74
Nigel Small <nigel@nigelsmall.com>
2014-07-06 00:12:21 +0100

    updated twitter feed

--- _twitter.php
+++ _twitter.php
@@ -1,5 +1,5 @@
 <div style="position:absolute;right:0">
-    <a class="twitter-timeline" width="364" height="600" href="https://twitter.com/technige" data-widget-id="278651081788096512" data-list-owner-screen-name="technige" data-list-slug="technology-feed" data-link-color="#0D884C" data-border-color="#DDD">Tweets by @technige</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<a class="twitter-timeline" width="364" height="600" href="https://twitter.com/neonige" data-widget-id="485561220078903296" data-link-color="#0D884C" data-border-color="#DDD">Tweets by @neonige</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
