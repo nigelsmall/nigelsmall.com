24db834fc3b1e3889631a5c31a2ffc505a0a6116
Nigel Small <nigel@nigelsmall.com>
2014-07-06 15:44:17 +0100

    fixed header link

--- _header.php
+++ _header.php
@@ -1,5 +1,5 @@
 <header>
-<div class="title"><a href=".">Nigel Small<!--img src="img/nigetech.400x80.png"--></a></div>
+<div class="title"><a href="/">Nigel Small</a></div>
 <div class="contact">
 <a href="https://twitter.com/neonige"><img src="/img/twitter_mark.png"></a>
 <a href="https://github.com/nigelsmall"><img src="/img/github_mark.png"></a>
